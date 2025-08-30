806827bf6025397263d1ba14af9cfa4624ffc243
Ericsipad <145693731+Ericsipad@users.noreply.github.com>
2025-08-30 11:12:22 -0600

    frontend(chopping-board): set 25-mode size to 120px

--- frontend/src/internal/components/ProfileGrid.tsx
+++ frontend/src/internal/components/ProfileGrid.tsx
@@ -21,7 +21,7 @@ export default function ProfileGrid(props: ProfileGridProps) {
 			wobble = true
 			break
 		case 25:
-			cardPx = 100
+			cardPx = 120
 			columns = 5
 			gap = '8px'
 			wobble = true
