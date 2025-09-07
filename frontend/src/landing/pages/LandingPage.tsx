13e24e27c968ed84ecb956a8ccb7ceb11fb22124
Ericsipad <145693731+Ericsipad@users.noreply.github.com>
2025-09-07 14:20:26 -0600

    feat(landing): use new second section image and add original as third

--- frontend/src/landing/pages/LandingPage.tsx
+++ frontend/src/landing/pages/LandingPage.tsx
@@ -5,6 +5,7 @@ export default function LandingPage() {
 	return (
 		<main className="landing-root">
 			<HeroSection />
+			<LowerImageSection imageUrl="https://publicwebassets.b-cdn.net/landing%20sec2.jpeg" />
 			<LowerImageSection />
 		</main>
 	)
