03c75fb568ba734daf39eb5c0372fe475b7079d7
Ericsipad <145693731+Ericsipad@users.noreply.github.com>
2025-09-17 20:48:29 -0600

    fix: remove stray escape characters in TopHeader

--- frontend/src/internal/components/TopHeader.tsx
+++ frontend/src/internal/components/TopHeader.tsx
@@ -3,15 +3,15 @@ import UserIndicator from './UserIndicator'
 import '../styles/internal.css'
 
 export default function TopHeader() {
-\treturn (
-\t\t<header className="internal-top-header">
-\t\t\t<Container>
-\t\t\t\t<div className="internal-top-header-inner">
-\t\t\t\t\t<div className="internal-user-indicator">
-\t\t\t\t\t\t<UserIndicator />
-\t\t\t\t\t</div>
-\t\t\t\t</div>
-\t\t\t</Container>
-\t\t</header>
-\t)
+	return (
+		<header className="internal-top-header">
+			<Container>
+				<div className="internal-top-header-inner">
+					<div className="internal-user-indicator">
+						<UserIndicator />
+					</div>
+				</div>
+			</Container>
+		</header>
+	)
 }
